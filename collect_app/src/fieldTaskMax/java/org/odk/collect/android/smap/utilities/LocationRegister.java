7e61ba2fa683309bc875676dff1f2867d9a88f48
Neil Penman <neilpenman@gmail.com>
2024-09-08 13:30:46 +1000

    add installation validation to max variant

--- collect_app/src/fieldTaskMax/java/org/odk/collect/android/smap/utilities/LocationRegister.java
+++ collect_app/src/fieldTaskMax/java/org/odk/collect/android/smap/utilities/LocationRegister.java
@@ -124,5 +124,84 @@ public class LocationRegister {
 
     // Check that the installation is good
     public void isValidInstallation(Context context) {
+        // Check that the installation is good
+        public void isValidInstallation(Context context) throws Exception {
+
+            /*
+             * Look for evidence of rooting on the file path
+             */
+            File suApk = new File("/system/app/Superuser.apk");
+            File suBin = new File("/system/bin/su");
+            File suBin2 = new File("/system/binx/su");
+
+            if(suApk.exists() || suBin.exists() || suBin2.exists()) {
+                throw new Exception(context.getString(R.string.smap_compromised));
+            }
+
+            /*
+             * Attempt to execute a super user command
+             */
+            Process p =  null;
+            try {
+                p = Runtime.getRuntime().exec("su");
+                if(p != null ) {
+                    throw new Exception(context.getString(R.string.smap_compromised));
+                }
+            } catch(IOException e) {
+                // OK
+            } finally {
+                if(p != null) {
+                    try {
+                        p.destroy();
+                    } catch (Exception e) {
+                        Timber.e(e);
+                    }
+                }
+            }
+
+            /*
+             * Check for the busy box package
+             */
+            String bbName = "stericson.busybox";
+            PackageManager pm = context.getPackageManager();
+            try {
+                PackageInfo pi = pm.getPackageInfo(bbName, PackageManager.GET_ACTIVITIES);
+                if(pi != null ) {
+                    throw new Exception(context.getString(R.string.smap_compromised));
+                }
+            } catch(IOException e) {
+                // OK
+            }
+
+            /*
+             * Check for running on an emulator
+             */
+            if (!BuildConfig.BUILD_TYPE.equals("debug")) {
+
+                if(Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+                        && Build.FINGERPRINT.endsWith(":user/release-keys")
+                        && Build.MANUFACTURER.equals("Google") && Build.PRODUCT.startsWith("sdk_gphone") && Build.BRAND.equals("google")
+                        && Build.MODEL.startsWith("sdk_gphone")
+                        || Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.HARDWARE.contains("goldfish")
+                        || Build.HARDWARE.contains("ranchu")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")
+                        || Build.MANUFACTURER.contains("Genymotion")
+                        || Build.HOST.equals("Build2") //MSI App Player
+                        || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                        || Build.PRODUCT.contains("sdk_google")
+                        || Build.PRODUCT.equals("google_sdk")
+                        || Build.PRODUCT.contains("sdk")
+                        || Build.PRODUCT.contains("sdk_x86")
+                        || Build.PRODUCT.contains("vbox86p")
+                        || Build.PRODUCT.contains("emulator")
+                        || Build.PRODUCT.contains("simulator")) {
+                    throw new Exception(context.getString(R.string.smap_emulator));
+                }
+            }
+        }
     }
 }
